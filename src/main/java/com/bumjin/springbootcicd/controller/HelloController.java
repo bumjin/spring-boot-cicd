e60a3741adf98738897ed71f3fa1080f186d1af5
bumjin <bumjin@gmail.com>
2023-09-29 15:57:21 +0900

    first commit

---
+++ src/main/java/com/bumjin/springbootcicd/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.bumjin.springbootcicd.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/welcome")
+    public String getMessage(){
+        return "Welcome to JiniLand";
+    }
+}
