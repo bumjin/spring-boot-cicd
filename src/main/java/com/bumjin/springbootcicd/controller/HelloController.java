66484a59d387183687f86f060c55ab1d0690acc1
bumjin <bumjin@gmail.com>
2023-10-02 16:02:54 +0900

    add sl4j logging feature

--- src/main/java/com/bumjin/springbootcicd/controller/HelloController.java
+++ src/main/java/com/bumjin/springbootcicd/controller/HelloController.java
@@ -2,10 +2,12 @@ package com.bumjin.springbootcicd.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @Tag(name = "RestController 예제 API", description = "Swagger 테스트용 API")
 @RequestMapping("/api")
 @RestController
@@ -14,6 +16,7 @@ public class HelloController {
     @Operation(summary = "환영인사", description = "환영인사를 출력합니다.")
     @GetMapping("/welcome")
     public String getMessage(){
+        log.debug("getMessage called");
         return "Welcome to JiniLand";
     }
 }
--- src/main/java/com/bumjin/springbootcicd/controller/HomeController.java
+++ src/main/java/com/bumjin/springbootcicd/controller/HomeController.java
@@ -2,10 +2,13 @@ package com.bumjin.springbootcicd.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@Slf4j
 @Tag(name = "컨트롤러 예제 API", description = "Swagger 테스트용 API")
 @Controller
 public class HomeController {
@@ -16,16 +19,18 @@ public class HomeController {
     }
 
     @Operation(summary = "jsp예제", description = "WEB-INF/jsp/main.jsp로 출력합니다.")
-    @GetMapping(value = "/jsp")
-    public String jsp(Model model) {
-        model.addAttribute("name", "jsp");
+    @GetMapping(value = "/jsp/{name}")
+    public String jsp(@PathVariable("name") String name, Model model) {
+        log.debug("jsp called name={}", name);
+        model.addAttribute("name", name);
         return "main";
     }
 
     @Operation(summary = "thymeleaf예제", description = "templates/thymeleaf/main.html로 출력합니다.")
-    @GetMapping(value = "/th")
-    public String th(Model model) {
-        model.addAttribute("name", "thymeleaf");
+    @GetMapping(value = "/th/{name}")
+    public String th(@PathVariable("name") String name, Model model) {
+        log.debug("thymeleaf called name={}", name);
+        model.addAttribute("name", name);
         return "thymeleaf/main";
     }
 // ...
